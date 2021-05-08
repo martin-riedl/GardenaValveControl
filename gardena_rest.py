56fc43955c86019bd3af4c96d97d6c16c77a0b68
Martin Riedl <martin.riedl@posteo.de>
2021-05-08 14:40:07 +0200

    gardena_rest.py

--- gardena_rest.py
+++ gardena_rest.py
@@ -10,70 +10,99 @@ import RPi.GPIO as GPIO
 from flask import Flask, render_template
 from threading import Timer
 from flask import jsonify
-
+from typing import Text
 app = Flask(__name__)
 
-valves = [{
-  "status" : "closed"
-}]
+valves_config = []
 
-def timeout():
-    print("closing timeout")
-    pure_close()
-    pure_close()
+valves_status = []
 
-def statusPage():
+def timeout(gpio_close):
+  """Timeout function which prints the closing event and for robustness applies the closing function multiple times."""
+
+  print("closing timeout")
+  pure_close(gpio_close=gpio_close)
+  pure_close(gpio_close=gpio_close)
+
+def statusPage() -> Text:
+  """Create the HTML status page of the valves.
+
+  Returns:
+      Text: The valve status page.
+  """
   templateData = {
-    'valves' : valves
+    'valves' : valves_status
   }
   return render_template('template.html', **templateData)
 
+def pure_open(gpio_open):
+  GPIO.output(gpio_open, GPIO.HIGH)
+  time.sleep(0.5)
+  GPIO.output(gpio_open, GPIO.LOW)
+  valves_status[0]["status"]="opened"
+  print(valves_status)
+
+def pure_close(gpio_close : int):
+  """Closes a valve.
+  Args:
+      gpio_close (int): The closing GPIO port of the desired valve. 
+  """
+  GPIO.output(gpio_close, GPIO.HIGH)
+  time.sleep(0.150)
+  GPIO.output(gpio_close, GPIO.LOW)
+  valves_status[0]["status"]="closed"
+  print(valves_status)
+
 @app.route("/")
 def hello():
+  """Simply show the status page of the valves."""
   return statusPage()
-
-def pure_open():
-  GPIO.output(open, GPIO.HIGH)
-  time.sleep(0.5)
-  GPIO.output(open, GPIO.LOW)
-  valves[0]["status"]="opened"
-  print(valves)
 
 @app.route("/status")
 def get_status():
-  return jsonify(valves)
+  """Shows a JSON representation of the valves status."""
+  return jsonify(valves_status)
 
 @app.route("/open")
-def def_open():
-  return p_open(lambda : Timer(5*60, timeout).start())
+@app.route("/open/<idx>")
+def def_open(idx=0):
+  """Performs a timebound opening of a valve and returns the updated status page."""
+  return p_open(idx, lambda : Timer(5*60, timeout).start())
 
 @app.route("/openinf")
-def p_open(exec_timer=lambda : None):
-  pure_open()
-  exec_timer()
+@app.route("/openinf/<idx>")
+def p_open(idx=0, exec_timer=lambda : None):
+  """Performs a boundless opening of a valve and returns the updated status page."""
+  if len(valves_config)>0: 
+    pure_open(gpio_open=valves_config[idx]["open"])
+    exec_timer(gpio_close=valves_config[idx]["close"])
+  
   return statusPage()
 
-def pure_close():
-  GPIO.output(close, GPIO.HIGH)
-  time.sleep(0.150)
-  GPIO.output(close, GPIO.LOW)
-  valves[0]["status"]="closed"
-  print(valves)
-
 @app.route("/close")
-def f_close():
-  pure_close()
+@app.route("/close/<idx>")
+def f_close(idx=0):
+  """Closes of a valve and returns the updated status page."""
+  if len(valves_config)>0:
+    pure_close(gpio_close=valves_config[idx]["close"])
+
   return statusPage()
 
 if __name__ == "__main__":
   print("Setup GPIO")
   GPIO.setmode(GPIO.BCM)
 
-  # define GPIO ports variables to open/close the valve
-  open=22
-  close=23
 
-  pins = [open,close] #on, off
+  valves_config.append({
+    "open" : 22,
+    "close" : 23
+  })
+  valves_status.append({
+    "status" : "closed"
+  })
+
+  # determine all gpio pins used
+  pins = [pin for pin in [valve_config.values() for valve_config in valves_config]]
 
   for pin in pins:
     GPIO.setup(pin, GPIO.OUT)
